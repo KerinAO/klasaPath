ff8f7953bb44f6bb3b77976a5ca8ceb858b18af8
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-04-05 12:41:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace klasaPath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string prviDirektorij = @"c:\prviDir";
+            string drugiDirektorij = @"c:\drugiDir";
+
+            string[] nazivi = new string[5];
+
+            if (!Directory.Exists(prviDirektorij))
+            {
+                Directory.CreateDirectory(prviDirektorij);
+            }
+            
+            if (!Directory.Exists (drugiDirektorij))
+            {
+                Directory.CreateDirectory (drugiDirektorij);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                nazivi[i] = "Datoteka" + i;
+                if (!File.Exists(@"c:\prviDir\" + nazivi[i]))
+                {
+                    File.Create(@"c:\prviDir\" + nazivi[i]);
+                }
+            }
+
+            try
+            {
+                foreach (string datoteka in Directory.GetFiles(prviDirektorij))
+                {
+                    string imeDatoteke = Path.GetFileName(datoteka);
+                    string ciljanaDatoteka = Path.Combine(drugiDirektorij, imeDatoteke);
+
+                    File.Copy(datoteka, ciljanaDatoteka, false);
+                }
+                Console.WriteLine("It is done!");
+            }
+            catch (Exception greska)
+            {
+                Console.WriteLine("Greška: {0}", greska.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
